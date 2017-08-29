dc77e4fc09b2a624cb50795fb37da79161fd5a08
Olafur Gislason <olafurgi@gmail.com>
2017-08-29 19:44:35 +0000

    Make menu path relative
    Support different names of the custom module

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
 <?php
 
 $ret['MODS'][] = MENU_DIVIDER;
-$ret['MODS'][] = 'Install IAC rules' . '|' . $CFG->ROOT_DIR . 'Modules/Custom/IACRules/install.php';
+$ret['MODS'][] = 'Install IAC rules' . '|' . $CFG->ROOT_DIR . 'Modules/Custom/' . basename(dirname(__FILE__)) . '/install.php';
