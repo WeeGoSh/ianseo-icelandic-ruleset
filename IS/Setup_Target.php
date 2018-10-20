da72b9171a8a378da96a2f886800ab16e0b36de8
Olafur Gislason <olafurgi@gmail.com>
2018-10-20 16:15:26 +0000

    Fix CN_ outdoor distance

--- IS/Setup_Target.php
+++ IS/Setup_Target.php
@@ -22,14 +22,14 @@ case 3:
     CreateDistance($TourId, $TourType, 'RM_', '60 m', '60 m'); // Recurve Master
     CreateDistance($TourId, $TourType, 'RJ_', '70 m', '70 m'); // Recurve Junior
     CreateDistance($TourId, $TourType, 'RC_', '60 m', '60 m'); // Recurve Cadet
-    CreateDistance($TourId, $TourType, 'RN_', '40 m', '40 m'); // Recurve Cadet
+    CreateDistance($TourId, $TourType, 'RN_', '40 m', '40 m'); // Recurve Nordic Cadet
     CreateDistance($TourId, $TourType, 'RB_', '60 m', '60 m'); // Recurve Beginner
 
     CreateDistance($TourId, $TourType, 'C_', '50 m', '50 m');  // Compound Open
     CreateDistance($TourId, $TourType, 'CM_', '50 m', '50 m'); // Compound Master
     CreateDistance($TourId, $TourType, 'CJ_', '50 m', '50 m'); // Compound Junior
     CreateDistance($TourId, $TourType, 'CC_', '50 m', '50 m'); // Compound Cadet
-    CreateDistance($TourId, $TourType, 'CN_', '40 m', '40 m'); // Compound Nordic Cadet
+    CreateDistance($TourId, $TourType, 'CN_', '30 m', '30 m'); // Compound Nordic Cadet
     CreateDistance($TourId, $TourType, 'CB_', '50 m', '50 m'); // Compound Beginner
 
     CreateDistance($TourId, $TourType, 'B_', '30 m', '30 m');  // Barebow Open
